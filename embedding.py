e0060dd3bc121e599710f67cb78db550f3b1b4d0
sherozshaikh <148150823+sherozshaikh@users.noreply.github.com>
2024-07-10 14:20:16 -0400

    Update embedding.py

--- embedding.py
+++ embedding.py
@@ -279,6 +279,11 @@ class TextEmbedding():
 if __name__ == "__main__":
 
     !pip install spacy spacy-transformers sentence-transformers laserembeddings gensim
+    !python -m laserembeddings download-models
+    !python -m spacy download en_core_web_sm
+    # !python -m spacy download en_core_web_md
+    # !python -m spacy download en_core_web_lg
+    # !python -m spacy download en_core_web_trf
 
     import torch
     from transformers import AutoTokenizer,AutoModel
